1876f2dc5c9b763587f95b342ef1ef83827c3c3a
BlackHead <olegvolodimirovic56@gmail.com>
2023-01-30 22:35:48 +0200

    do hm1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import {Simpsons} from "./components/Simpsons/Simpsons";
+import {Characters} from "./components/Characters/Characters";
+
 const App = () => {
     return (
         <div>
-            App
+            <Simpsons/>
+            <Characters/>
         </div>
     );
 };
 
-export {App};
+export  {App};
---
+++ src/components/Simpsons/Simpsons.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Simpsons = () => {
+    return (
+        <div>
+            <div>
+                <div>Name: Homer </div>
+                <div>Surname: Simpson </div>
+                <img src="https://upload.wikimedia.org/wikipedia/uk/0/02/Homer_Simpson_2006.png"/>
+            </div>
+
+            <div>
+                <div>Name: Marge </div>
+                <div>Surname: Simpson </div>
+                <img src="https://upload.wikimedia.org/wikipedia/ru/thumb/0/0b/Marge_Simpson.png/217px-Marge_Simpson.png"/>
+            </div>
+
+            <div>
+                <div>Name: Bart </div>
+                <div>Surname: Simpson </div>
+                <img src="https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png"/>
+            </div>
+
+            <div>
+                <div>Name: Lisa </div>
+                <div>Surname: Simpson </div>
+                <img src="https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png"/>
+            </div>
+
+            <div>
+                <div>Name: Maggie </div>
+                <div>Surname: Simpson </div>
+                <img src="https://static.simpsonswiki.com/images/thumb/9/9d/Maggie_Simpson.png/250px-Maggie_Simpson.png"/>
+            </div>
+        </div>
+    );
+};
+
+export  {Simpsons};
